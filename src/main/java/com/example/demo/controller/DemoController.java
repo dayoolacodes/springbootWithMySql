d9160a736b3b1652dffe6ca781c30fb3f224b8fb
dayoola <ayodayo1@gmail.com>
2020-08-08 09:23:13 +0100

    wip

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,15 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.db.User;
 import com.example.demo.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-@Controller
+import java.util.List;
+
+@RestController
+//@Controller
+//@ResponseBody
 //@RequestMapping("/url")
 public class DemoController {
 
@@ -25,6 +30,11 @@ public class DemoController {
     public String home(Model model) {
         model.addAttribute("users", userRepo.findAll());
         return "index";
+    }
+
+    @RequestMapping("/1")
+    public List<User> home1(Model model) {
+                return userRepo.findAll();
     }
 
 }
